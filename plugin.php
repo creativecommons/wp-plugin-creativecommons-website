8001dc89c9817b9e5b76916a20239ae85662373e
Hugo Solar <hugo.solar@gmail.com>
2020-09-28 15:29:15 -0300

    update plugin information

--- plugin.php
+++ plugin.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Plugin Name: vocabulary-blocks — CGB Gutenberg Block Plugin
+ * Plugin Name: Vocabulary blocks 
  * Plugin URI: https://github.com/ahmadawais/create-guten-block/
- * Description: vocabulary-blocks — is a Gutenberg plugin created via create-guten-block.
- * Author: mrahmadawais, maedahbatool
- * Author URI: https://AhmadAwais.com/
- * Version: 1.0.0
- * License: GPL2+
- * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+ * Description: Vocabulary blocks is a collection of CC Vocabulary components as Gutenberg blocks
+ * Author: Creative Commons
+ * Author URI: https://creativecommns.org
+ * Version: 2020.09.1
+ * License: MIT
+ * License URI: https://github.com/creativecommons/wp-plugin-vocabulary-blocks/blob/master/LICENSE
  *
  * @package CGB
  */
