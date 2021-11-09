826a89ebb1bc71439c4b051b2e4db3a417d15ca2
Shailee Mehta <mehtashailee21@gmail.com>
2021-11-09 21:04:05 +0530

    [#72] Updating css and html placement. Making sure dropdown element is not visible in preview mode

--- blocks/users-list/edit.js
+++ blocks/users-list/edit.js
@@ -1,31 +1,21 @@
 import ServerSideRender from '@wordpress/server-side-render';
-import { useSelect } from '@wordpress/data';
+import { useEffect } from 'react';
+import { useBlockProps } from '@wordpress/block-editor';
 
 export default function Edit( { attributes, setAttributes } ) {
-	const userGroups = useSelect( ( select ) => {
-		return select( 'core' ).getEntityRecords( 'taxonomy', 'group' );
-	}, [] );
-
-	/* Display all group names */
-	const changeGroupName = ( role ) => {
-		if ( ! role ) {
-			setAttributes( { selectedValue: null } );
-			return;
-		}
-		const selectedValue = parseInt( role.target.value );
-		setAttributes( { selectedValue } );
-	};
-
-	return ( <div>
-		<label htmlFor="terms">Choose a role: </label>
-		<select name="terms" onBlur={ ( r )=>changeGroupName( r ) } id="terms">
-			<option value="">All</option>
-
-			{ userGroups && userGroups.map( function( taxo, i ) {
-				return <option key={ taxo.id } value={ taxo.id }>{ taxo.name }</option>;
-			} ) }
-
-		</select>
+	const blockProps = useBlockProps();
+	useEffect( () => {
+		const readyStateCheckInterval = setInterval( function() {
+			if ( document.querySelector( '#terms' ) ) {
+				clearInterval( readyStateCheckInterval );
+				const terms = document.querySelector( '#terms' );
+				terms.addEventListener( 'blur', ( e ) => {
+					setAttributes( { selectedValue: e.target.value } );
+				} );
+			}
+		}, 10 );
+	} );
+	return ( <div { ...blockProps }>
 		<ServerSideRender
 			key={ attributes.selectedValue }
 			block="common/users-list"
